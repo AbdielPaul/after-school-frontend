dd0e967d4f248e0dcd2b38d206a1dc133969b642
AbdielPaul <abdielpaul29@gmail.com>
2025-11-22 17:22:42 +0400

    Updated Lessons List

--- lessons.js
+++ lessons.js
@@ -2,7 +2,7 @@ let products = [
   {
     "id": 1001,
     "subject": "Football",
-    "location": "Al Khalifa Stadium",
+    "location": "Sports City",
     "price": 20.00,
     "image": "images/football.png",
     "spaces": 5,
@@ -11,37 +11,82 @@ let products = [
   {
     "id": 1002,
     "subject": "Basketball",
-    "location": "Al Gharafa Stadium",
-    "price": 2.99,
-    "image": "images/swimming.png",
+    "location": "Sports City",
+    "price": 15.50,
+    "image": "images/Basketball.png",
     "spaces": 7,
     "rating": 4
   },
   {
     "id": 1003,
-    "subject": "Cricket",
-    "location": "Sport city stadium",
-    "price": 2.99,
-    "image": "images/yarn.png",
+    "subject": "Creative Writing",
+    "location": "Mohammed bin Rashid Library",
+    "price": 5.99,
+    "image": "images/writing.png",
     "spaces": 15,
-    "rating": 4
+    "rating": 5
   },
   {
     "id": 1004,
-    "subject": "Swimming",
-    "location": "Hamad Aquatic center",
-    "price": 2.99,
-    "image": "images/yarn.png",
-    "spaces": 17,
+    "subject": "Robotics",
+    "location": "Al Barhsa",
+    "price": 25.00,
+    "image": "images/robotics.png",
+    "spaces": 12,
     "rating": 4
   },
   {
     "id": 1005,
-    "subject": "Tennis",
-    "location": "Doha Tennis Academy",
-    "price": 3.99,
-    "image": "images/yarn.png",
+    "subject": "Art & Painting",
+    "location": "Downtown",
+    "price": 18.75,
+    "image": "images/painting.png",
     "spaces": 17,
+    "rating": 5
+  },
+  {
+    "id": 1006,
+    "subject": "Drama & Acting",
+    "location": "Abu Shagara ",
+    "price": 12.99,
+    "image": "images/drama.png",
+    "spaces": 8,
+    "rating": 3
+  },
+  {
+    "id": 1007,
+    "subject": "Chess",
+    "location": "Dubai International Academic City",
+    "price": 4.50,
+    "image": "images/chess.png",
+    "spaces": 20,
+    "rating": 4
+  },
+  {
+    "id": 1008,
+    "subject": "Guitar Training",
+    "location": "Discovery Gardens",
+    "price": 30.00,
+    "image": "images/guitar.png",
+    "spaces": 6,
+    "rating": 5
+  },
+  {
+    "id": 1009,
+    "subject": "Gardening",
+    "location": "Al Majaz",
+    "price": 7.00,
+    "image": "images/gardening.png",
+    "spaces": 14,
+    "rating": 3
+  },
+  {
+    "id": 1010,
+    "subject": "Coding Fundamentals",
+    "location": "Al Karama",
+    "price": 22.50,
+    "image": "images/coding.png",
+    "spaces": 10,
     "rating": 4
   }
 ];
