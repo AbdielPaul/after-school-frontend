93f9c6809615ea00f21b130e98b48d4f980138b2
AbdielPaul <abdielpaul29@gmail.com>
2025-11-22 16:36:17 +0400

    Checkout page

---
+++ lessons.js
@@ -0,0 +1,47 @@
+let products = [
+  {
+    "id": 1001,
+    "subject": "Football",
+    "location": "Al Khalifa Stadium",
+    "price": 20.00,
+    "image": "images/football.png",
+    "spaces": 5,
+    "rating": 3
+  },
+  {
+    "id": 1002,
+    "subject": "Basketball",
+    "location": "Al Gharafa Stadium",
+    "price": 2.99,
+    "image": "images/swimming.png",
+    "spaces": 7,
+    "rating": 4
+  },
+  {
+    "id": 1003,
+    "subject": "Cricket",
+    "location": "Sport city stadium",
+    "price": 2.99,
+    "image": "images/yarn.png",
+    "spaces": 15,
+    "rating": 4
+  },
+  {
+    "id": 1004,
+    "subject": "Swimming",
+    "location": "Hamad Aquatic center",
+    "price": 2.99,
+    "image": "images/yarn.png",
+    "spaces": 17,
+    "rating": 4
+  },
+  {
+    "id": 1005,
+    "subject": "Tennis",
+    "location": "Doha Tennis Academy",
+    "price": 3.99,
+    "image": "images/yarn.png",
+    "spaces": 17,
+    "rating": 4
+  }
+];
